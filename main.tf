9590419ad956c2766afe07faf407e02fc499e596
devendraSinghtomar24 <99275680+devendraSinghtomar24@users.noreply.github.com>
2025-07-06 18:26:53 +0530

    Ritm1000 (#1)
    * add rg notes
    
    * add another RG
    
    * Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,14 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "example" {
-    name     = "example-resource-group"
+resource "azurerm_resource_group" "rg-01" {
+    name     = "hfcl-dev-01"
     location = "East US"
 }
+
+resource "azurerm_resource_group" "rg02" {
+    name     = "hfcl-dev-02"
+    location = "East US"
+}
+
+# this is for new RG
