83ef87cd8cec8c29df7d80d859a6265550d69365
devendraSinghtomar24 <99275680+devendraSinghtomar24@users.noreply.github.com>
2025-07-06 19:02:35 +0530

    add rg (#2)
    add to rg

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@ resource "azurerm_resource_group" "rg-01" {
     location = "East US"
 }
 
-resource "azurerm_resource_group" "rg02" {
+resource "azurerm_resource_group" "rg-02" {
+    name     = "hfcl-dev-02"
+    location = "East US"
+}
+
+resource "azurerm_resource_group" "rg-03" {
     name     = "hfcl-dev-02"
     location = "East US"
 }
